25d541f235edf260a940696dc908af7512a1a770
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2021-03-03 20:48:08 -0300

    side-bar nav layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { CountdownProvider } from "../contexts/CountdownContext";
 import { ChallengesProvider } from "../contexts/ChallengesContext";
 
+import SideBar from "../components/SideBar"
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
@@ -20,9 +21,7 @@ interface HomeProps {
   challengesCompleteds: number;
 }
 
-
-export default function Home(props: HomeProps) {  
-
+export default function Home(props: HomeProps) { 
   useEffect(() => {
     const url = window.location.href;
     const newUrl = url.split("?token_type=");
@@ -35,26 +34,28 @@ export default function Home(props: HomeProps) {
       currentExperience={props.currentExperience}
       challengesCompleteds={props.challengesCompleteds}
     >
-      <div className={styles.container}>   
+      <div className={styles.wrap}>
         <Head>
           <title>Inicio | Move IT!</title>
-        </Head>   
-        <ExperienceBar/>
-
-        <CountdownProvider>      
-          <section>
-            <div>              
-              <Profile/>
-              <CompletedChallenges/>
-              <Countdown />
-            </div>
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountdownProvider>
-        
-      </div>  
+        </Head>  
+        <SideBar/>
+        <div className={styles.container}>   
+          <ExperienceBar/>
+          <CountdownProvider>      
+            <section>
+              <div>              
+                <Profile/>
+                <CompletedChallenges/>
+                <Countdown />
+              </div>
+              <div>
+                <ChallengeBox />
+              </div>
+            </section>
+          </CountdownProvider>        
+        </div>
+      </div> 
+       
     </ChallengesProvider>
   )
 }
---
+++ src/pages/leaderboard.tsx
@@ -0,0 +1,17 @@
+import SideBar from "../components/SideBar"
+import Head from "next/head";
+import styles from "../styles/pages/leaderboard.module.css"
+
+export default function Leaderboard() {
+  return (
+    <div className={styles["leaderboard-wrap"]}>
+      <Head>
+        <title>Leaderboard | Move IT!</title>
+      </Head> 
+      <SideBar/>
+      <div className={styles["leaderboard-container"]}> 
+        <p>Leaderboard page</p>
+      </div>
+    </div>
+  )
+}
