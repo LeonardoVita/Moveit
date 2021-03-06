0694c8b044032cd87dc75a58587950a0a0288ad9
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2021-03-06 00:59:35 -0300

    login github button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,44 +1,28 @@
-import { useState } from "react";
 import { getSession, signIn} from "next-auth/client";
 
 import styles from "../styles/pages/login.module.css";
 
 
 export default function login() {  
-  const [ login, setLogin] = useState("");
 
   return (
     <div className={styles["login-container-background"]}>
       <div className={styles["login-container"]}>
         <img src="logo-full.svg" alt="moveit logo"/>  
         <strong>Bem-vindo</strong>
-        <div className={styles["github-span-container"]}>
+        <button 
+          className={styles["github-button"]}
+          onClick={(): Promise<void> =>
+            signIn("github")
+          }
+        >
           <img src="/icons/Github.svg" alt="github icone"/>
           <span>Faça Login com seu Github para começar</span>
-        </div>
-        <div className={styles["login-input-container"]}>
-          <input 
-            type="text" 
-            name="login" 
-            id="login" 
-            placeholder="Github User login"
-            value={login} 
-            onChange={(e) => setLogin(e.target.value)}
-          /> 
-               
-          <button
-            onClick={(): Promise<void> =>
-              signIn("github")
-            }
-          >
-            <img src="/icons/right-arrow.svg" alt="seta para direita icone"/>
-          </button>          
-        </div>
+        </button>                
       </div>
     </div>
   );
 }
-
 
 export const getServerSideProps = async ({ req, res }) => {
 
